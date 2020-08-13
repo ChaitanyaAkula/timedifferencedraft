7b28e31420fafbd382982570ad34c2271fe9ba66
ChaitanyaAkula <krishchaituakula@gmail.com>
2020-08-13 06:31:13 +0530

    Update timedifferencedraft.go

--- timedifferencedraft.go
+++ timedifferencedraft.go
@@ -19,54 +19,54 @@ func TimeDifferencedraft(postdate string)string{
 			min:=diff.Hours()*60
 			if int(min)==1{
 			minutes:=strconv.Itoa(int(min))
-			return "created "+minutes+" minute ago"
+			return "Created "+minutes+" minute ago"
 			}
 			if int(min)>1{
 				minutes:=strconv.Itoa(int(min))
-				return "created "+minutes+" minutes ago"
+				return "Created "+minutes+" minutes ago"
 				}
 			if int(min)<1{
-				return "created a few seconds ago"
+				return "Created a few seconds ago"
 			}
 		}
 		if timediff==1{
 			hours:=strconv.Itoa(timediff)
-			return "created "+hours+" hour ago"
+			return "Created "+hours+" hour ago"
 		}
 		if timediff>1{
 			hours:=strconv.Itoa(timediff)
-			return "created "+hours+" hours ago"
+			return "Created "+hours+" hours ago"
 		}
 	}
 	if timediff>24{
 		days:=timediff/24
 		if days==1{
 			nofdays:=strconv.Itoa(days)
-			return "created "+nofdays+" day ago"
+			return "Created "+nofdays+" day ago"
 		}
 		if days<=30&&days!=1{
 		nofdays:=strconv.Itoa(days)
-		return "created "+nofdays+" days ago"
+		return "Created "+nofdays+" days ago"
 	}
 	if days>30{
 		months:=days/30
 		if months==1{
 			nofmonths:=strconv.Itoa(months)
-			return "created "+nofmonths+" month ago"
+			return "Created "+nofmonths+" month ago"
 			}
 		if months<12&&months!=1{
 		nofmonths:=strconv.Itoa(months)
-		return "created "+nofmonths+" months ago"
+		return "Created "+nofmonths+" months ago"
 		}
 		if months>=12{
 			years:=months/12
 			nofyears:=strconv.Itoa(years)
 			if years==1{
 				
-			return "created "+nofyears+" year ago"
+			return "Created "+nofyears+" year ago"
 			}
 			if years>1{
-				return "created "+nofyears+" years ago"
+				return "Created "+nofyears+" years ago"
 			}
 		}
 	}
